2f97ae956114421948087d79fcbcd369b59c1b00
carolina.silva <aanacarolina@gmail.com>
2022-11-04 14:54:18 -0300

    adding bank functions and tests

--- test/exercism/bank_test.clj
+++ test/exercism/bank_test.clj
@@ -1,14 +1,20 @@
 (ns exercism.bank-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test :refer [deftest is]]
             [exercism.bank :as bank]))
 
 
 (deftest create-account-test
   (is (= {:accounts {0 {:balance 0}}}
          (bank/create-account {} 0)))
+
   (is (= {:accounts {0 {:balance 10}
                      1 {:balance 0}}}
          (bank/create-account {:accounts {0 {:balance 10}}} 1))))
 
+(deftest make-deposit-test
+  (is (= {:accounts {0 {:balance 10}}}
+         (bank/make-deposit {:accounts {0 {:balance 0}}} 0 10)))
+  (is (= {:accounts {0 {:balance 35}}}
+         (bank/make-deposit {:accounts {0 {:balance 10}}} 0 25))))
 
 
