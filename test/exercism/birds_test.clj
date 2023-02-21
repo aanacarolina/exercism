29694ae38d58163eeed087ff562719f0ae3cc282
carolina.silva <aanacarolina@gmail.com>
2023-02-21 10:56:43 -0300

    birds final

---
+++ test/exercism/birds_test.clj
@@ -0,0 +1,31 @@
+(ns exercism.birds-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [exercism.birds :as birds])
+  (:import (clojure.lang ExceptionInfo)))
+
+
+
+(deftest odd-week-test
+  (testing "First item is 1"
+    (is (= true
+           (birds/odd-week? [1 0 1 0 1 0 1]))))
+  (testing "First item is 0"
+    (is (= true
+           (birds/odd-week? [0 1 0 1 0 1 0]))))
+  (testing "All values are 1"
+    (is (= false
+           (birds/odd-week? [1 1 1 1 1 1 1])))))
+
+
+(deftest new-odd-week-test
+  (testing "First item is 1"
+    (is (= true
+           (birds/new-odd-week? [1 0 1 0 1 0 1]))))
+  (testing "First item is 0"
+    (is (= true
+           (birds/new-odd-week? [0 1 0 1 0 1]))))
+  (testing "All values are 1"
+    (is (= false
+           (birds/new-odd-week? [1 1 1 1 1 1 1])))))
+
+
