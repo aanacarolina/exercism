2e3b0db4c19b74dfd0bd9e7666c226043853dc8a
carolina.silva <aanacarolina@gmail.com>
2023-06-21 13:10:34 -0300

    full alphabet map

--- src/exercism/alpha_mirror.clj
+++ src/exercism/alpha_mirror.clj
@@ -13,6 +13,19 @@
                    \k \p
                    \l \o
                    \m \n
+                   \n \m
+                   \o \l
+                   \p \k
+                   \q \j
+                   \r \i
+                   \s \h
+                   \t \g
+                   \u \f
+                   \v \e
+                   \w \d
+                   \x \c
+                   \y \b
+                   \z \a
                    \A \Z
                    \B \Y
                    \C \X
@@ -25,7 +38,20 @@
                    \J \Q
                    \K \P
                    \L \O
-                   \M \N})
+                   \M \N
+                   \N \M
+                   \O \L
+                   \P \K
+                   \Q \J
+                   \R \I
+                   \S \H
+                   \T \G
+                   \U \F
+                   \V \E
+                   \W \D
+                   \X \C
+                   \Y \B
+                   \Z \A})
 
 (defn mirror-char [char]
   (get mirror-chars char char))
