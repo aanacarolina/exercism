c329a3528fe43c941c3d2361b2a646d1fc12aaef
carolina.silva <aanacarolina@gmail.com>
2023-07-19 11:03:51 -0300

    linkedlist neto

--- src/exercism/linked_list.clj
+++ src/exercism/linked_list.clj
@@ -1,5 +1,4 @@
-(ns exercism.linked-list
-  (:require [clojure.test :refer [deftest is]]))
+(ns exercism.linked-list)
 
 ;ideal for stack and add at the top (beginning)
 ;the end is the beginning (we actually start from the end)
@@ -19,22 +18,24 @@
    2.2 em seguida altera a referencia do node anterior pra o node criado)
 
 
+
+
 (defn linked-list [head tail] 
-  (conj tail head))
+  {:head head :tail tail})
 
 (defn head [llist]
-  (first llist))
+  (:head llist))
 
 (defn tail [llist]
-  (rest llist))
+  (:tail llist))
 
-(defn v-linked-list [head tail] ;;use vector
+#_(defn v-linked-list [head tail] ;;use vector
   (vector tail head))
 
-(defn v-head [llist]
+#_(defn v-head [llist]
   (subvec llist 0 1))
 
-(defn v-tail [llist]
+#_(defn v-tail [llist]
   (subvec llist 1))
 
 ;get last node (subvec [1 2 3 4] (- (count [1 2 3 4]) 1))
@@ -45,14 +46,77 @@
 ;;inserir ligar com o prox
 ;;trocar linha do head para o endereco do novo head
 
-(let [subject (v-linked-list 42 nil)]
-  (assert (= 42 (v-head subject))))
-(let [subject (v-linked-list 30 nil)]
-  (assert (= 30 (v-head subject))))
+(let [subject (linked-list 42 nil)]
+  (assert (= 42 (head subject))))
+(let [subject (linked-list 30 nil)]
+  (assert (= 30 (head subject))))
 
-(let [subject (v-linked-list 42 nil)
-      subject (v-linked-list 30 subject) 
-      _ (assert (= 30 (v-head subject))) 
-      subject (v-tail subject)
-      _ (assert (= 42 (v-head subject)))])
+(let [lista-1 (linked-list 42 nil)
+      lista-2 (linked-list 30 lista-1) 
+      _ (assert (= 30 (head lista-2))) 
+      lista-1b (tail lista-2)
+      _ (assert (= 42 (head lista-1b)))
+      lista-3 (linked-list 20 lista-2)
+      _ (assert (= 20 (head lista-3)))
+      
+      ])
 
+(def lista-1 {:head 42 :tail nil})
+ 
+(def lista-2 {:head 30 :tail lista-1})
+
+(def lista-3 {:head 20 :tail lista-2})
+
+(defn create-node [head]
+  {:head head :tail nil})
+
+(defn add-list [list head]
+  (if (nil? (:tail list)) 
+    (assoc  list :tail (create-node head))
+    (assoc list :tail (add-list (:tail list) head))))
+
+(defn add-nth 
+  ([list head position] (add-nth list head position 0) )
+  ([list head position current-position]
+   (if (= (dec position) current-position)
+     (let [new-node (create-node head)
+           new-node-tail (assoc new-node :tail (:tail list))]
+       (assoc list :tail new-node-tail))
+     (assoc list :tail (add-nth (:tail list) head position (inc current-position))))))
+
+;; cria node
+;;altera o tail do novo node recebe tail da lista e o tail da lista recebe o novo node
+{:head 3 :tail {:head 20, :tail nil}}
+{:head 42, :tail {:head 30, :tail{:head 3 :tail {:head 20, :tail nil}}}}
+
+;; CONTRARIO
+;; altera o tail recursivamente, procurando a posicao até achar , por inc no current position 
+
+;;count map = reduce by keys
+
+
+(defn count-list [list current]
+  (println "current: "current)
+  (if (nil? (:tail list)) current
+      (count-list (:tail list) (inc current))))
+
+(def lista-v1 [42 nil])
+(def lista-v2 [30 lista-v1])
+(def lista-v3 [20 lista-v2])
+
+;implement using vector
+ ;change nth element 
+
+
+(defn create-list [head] 
+  (conj '() head nil))
+
+(defn adding-node [list data]
+  (conj list data))
+
+(defn adding-node [list data]
+  (conj list data))
+
+
+(defn change-nth [list]
+  ())
