77062e84785f416ee2b378e720d3a60e5f4b3dd9
carolina.silva <aanacarolina@gmail.com>
2023-07-26 13:42:25 -0300

    my own flatten

--- src/exercism/linked_list.clj
+++ src/exercism/linked_list.clj
@@ -20,7 +20,7 @@
 
 
 
-(defn linked-list [head tail] 
+(defn linked-list [head tail]
   {:head head :tail tail})
 
 (defn head [llist]
@@ -30,13 +30,13 @@
   (:tail llist))
 
 #_(defn v-linked-list [head tail] ;;use vector
-  (vector tail head))
+    (vector tail head))
 
 #_(defn v-head [llist]
-  (subvec llist 0 1))
+    (subvec llist 0 1))
 
 #_(defn v-tail [llist]
-  (subvec llist 1))
+    (subvec llist 1))
 
 ;get last node (subvec [1 2 3 4] (- (count [1 2 3 4]) 1))
 
@@ -52,16 +52,15 @@
   (assert (= 30 (head subject))))
 
 (let [lista-1 (linked-list 42 nil)
-      lista-2 (linked-list 30 lista-1) 
-      _ (assert (= 30 (head lista-2))) 
+      lista-2 (linked-list 30 lista-1)
+      _ (assert (= 30 (head lista-2)))
       lista-1b (tail lista-2)
       _ (assert (= 42 (head lista-1b)))
       lista-3 (linked-list 20 lista-2)
-      _ (assert (= 20 (head lista-3))) 
-      ])
+      _ (assert (= 20 (head lista-3)))])
 
 (def lista-1 {:head 42 :tail nil})
- 
+
 (def lista-2 {:head 30 :tail lista-1})
 
 (def lista-3 {:head 20 :tail lista-2})
@@ -70,12 +69,12 @@
   {:head head :tail nil})
 
 (defn add-list [list head]
-  (if (nil? (:tail list)) 
+  (if (nil? (:tail list))
     (assoc  list :tail (create-node head))
     (assoc list :tail (add-list (:tail list) head))))
 
-(defn add-nth 
-  ([list head position] (add-nth list head position 0) )
+(defn add-nth
+  ([list head position] (add-nth list head position 0))
   ([list head position current-position]
    (if (= (dec position) current-position)
      (let [new-node (create-node head)
@@ -86,7 +85,7 @@
 ;; cria node
 ;;altera o tail do novo node recebe tail da lista e o tail da lista recebe o novo node
 {:head 3 :tail {:head 20, :tail nil}}
-{:head 42, :tail {:head 30, :tail{:head 3 :tail {:head 20, :tail nil}}}}
+{:head 42, :tail {:head 30, :tail {:head 3 :tail {:head 20, :tail nil}}}}
 
 ;; CONTRARIO
 ;; altera o tail recursivamente, procurando a posicao até achar , por inc no current position 
@@ -94,12 +93,12 @@
 ;;count map = reduce by keys
 
 (defn count-list [list current]
-  (println "current: "current)
+  (println "current: " current)
   (if (nil? (:tail list)) current
       (count-list (:tail list) (inc current))))
 
 (def lista-v1 [42 nil])
-(def lista-v2 [30 lista-v1]) 
+(def lista-v2 [30 lista-v1])
 (def lista-v3 [20 lista-v2]) ;;[20 [30 [42 nil]]]
 
 
@@ -122,26 +121,41 @@
 ;(get (get [20 [30 [42 nil]]] 1) 1)
 
 ;;implement our own flatten
-(defn flatten-v [list]
-  (flatten list))
+#_(defn flatten-v [list]
+    (when-not (nil? (reduce vector list))
+      flatten-v))
 
+(defn flatten-v [llist]
+  (loop [llist llist
+         flattened []]
+    (if (empty? llist)
+      flattened
+      (let [head (head-v llist)
+            tail (tail-v llist)] 
+        (recur tail
+               (conj flattened head))))))
+
+
+;(get-in list (count list))
+;(reduce conj [] list)
+;(reduce into [] list)
 ;apply str [20 30 43]
 ;em cada [20 [30 [42 nil]]]
 
 
 (let [subject (linked-list-v 42 nil)]
   (assert (= 42 (head-v subject))))
-(let [subject (linked-list-v 30 nil)]
-  (assert (= 30 (head-v subject))))
+(let [subject (linked-list-v nil nil)]
+  (assert (= nil (head-v subject))))
 
 (let [lista-1 (linked-list-v 42 nil)
-      lista-2 (linked-list-v 30 lista-1)
-      _ (assert (= 30 (head-v lista-2)))
+      lista-2 (linked-list-v nil lista-1)
+      _ (assert (= nil (head-v lista-2)))
       lista-1b (tail-v lista-2)
       _ (assert (= 42 (head-v lista-1b)))
       lista-3 (linked-list-v 20 lista-2)
       _ (assert (= 20 (head-v lista-3)))
-      _ (println (flatten-v lista-3))
-      _ (assert (= "203042" (apply str (flatten-v lista-3))))])
+      _ (println "LISTA-3 flat:" (flatten-v lista-3))
+      _ (assert (= [20 nil 42] (flatten-v lista-3)))])
 
 
